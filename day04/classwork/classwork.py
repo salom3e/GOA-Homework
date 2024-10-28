82483076cf59fd354c3d103faf7c74a43b167fe5
salom3e <salomemaisuradze2010@gmail.com>
2024-10-28 21:29:39 +0400

    second commit

---
+++ day04/classwork/classwork.py
@@ -0,0 +1,62 @@
+#string
+name = "salome"
+favmovie = "Interstellar"
+favmovie2 = "Saw"
+favband = "Korn"
+favbrand = "vivienne westwood"
+
+#integer
+age = 14
+favnumber = 26
+birth_month = 2 
+int1 = 99
+int2 = 34
+
+#float
+height = 58.8
+float1 = 66.9
+float2 = 0.3
+float3 = 000.06
+float4 = 6.0
+
+print(name, favmovie, favmovie2, favband, favbrand)
+print(age, favnumber, birth_month, int1, int2)
+print(height, float1, float2, float3, float4)
+ 
+print (47 + 9)
+print (67 + 8)
+print (7 + 19)
+print (8 + 6)
+print (409 + 68)
+
+print (78 - 8)
+print (98 - 97)
+print (7899 - 677)
+print (8000 - 7999)
+print (9006 - 6009)
+
+print (2 * 4)
+print (6 * 9)
+print (89 * 98)
+print (7 * 7)
+print (26 * 26)
+
+print (4 // 2)
+print (8 //4)
+print (9 // 3)
+print (49 // 7)
+print (55 // 5)
+
+print (56 % 5)
+print (67 % 8)
+print (78 % 9)
+print (99 % 4)
+print (87 % 6)
+
+print (4 ** 5)
+print (6 ** 2)
+print (7 ** 2)
+print (3 ** 9)
+print (10 ** 5)
+
+print (7 + 9, 98 - 97, 7 * 7, 49 // 7, 17 % 5, 10 ** 5)
