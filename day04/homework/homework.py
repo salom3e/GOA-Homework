10166a68ac0ae84c0f71191c7241ea1ee5ae841a
salom3e <salomemaisuradze2010@gmail.com>
2024-10-28 23:17:01 +0400

    second commit

---
+++ day04/homework/homework.py
@@ -0,0 +1,31 @@
+print (45 + 55)
+print (127 + 73)
+print (99 + 201)
+
+print (464 - 164)
+print (331 - 131)
+print (278 - 178)
+
+print (4 * 7)
+print (16 * 49)
+print (11 * 3)
+
+print (81 // 9)
+print (64 // 8)
+print (225 // 15)
+
+print (43 % 6)
+print (87 % 9)
+print (36 % 3)
+
+print (4 ** 2)
+print (2 ** 3)
+print (8 ** 5)
+
+mom = 43
+dad = 45
+me = 14
+grandma = 72 
+grandpa = 78
+
+print (mom + dad + me + grandma + grandpa)
