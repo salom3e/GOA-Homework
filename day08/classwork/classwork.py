beaaab2494dbff2ad54b0a1f070ba72e06359b11
salom3e <salomemaisuradze2010@gmail.com>
2024-11-27 22:32:37 +0400

    fifth commit

---
+++ day08/classwork/classwork.py
@@ -0,0 +1,16 @@
+name=input("hello. enter your name: ")
+
+print(name)
+
+
+n1=input("jig")
+n2=input("saw")
+
+print(n1 + n2)
+
+#input არის ინფორმაციის შეყვანა მოწყობილებაში ასე ვთქვათ, მაგ: მიკროფონი
+#output კი პირიქით, რაიმე ინფორმაცია რომ გამოდის. 
+
+
+
+
---
+++ day08/homework/homework.py
@@ -0,0 +1,13 @@
+print("entered program")
+name=input("what's your name? : ")
+print("hello,"+ " " + name)
+
+surname=input("I will also ask you your last name : ")
+print(surname)
+age=str(input("what is your age? : "))
+print(age)
+
+height=str(input("what is your height? : "))
+print("my height is" + " " + height + " " + "centimeters")
+
+print(name + " " + surname + " " + age + " " + "years old" + " " + height + " " + "centimeters")
