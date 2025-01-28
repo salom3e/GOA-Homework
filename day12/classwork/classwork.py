4d1425be4082618a5a57fdcbb48f37fbc4b6c0df
salomemaisuradze2010@gmail.com <salom3e>
2025-01-28 22:24:08 +0400

    day16

--- day12/classwork/classwork.py
+++ day12/classwork/classwork.py
@@ -4,7 +4,7 @@ num3=12
 print(num1 + num2 > num3) #True
 print(num1 - num2 < num3) #True
 print(num1 * num3 >= num2) #True
-print(num2 // num3 <=num1) #True
+print(num2 // num3 <= num1) #True
 print(num3 % num1 == num2) #False
 print(num2 ** num3 != num1) #True
 
@@ -14,7 +14,7 @@ age3=18
 print(age1 + age2 > age3) #True
 print(age1 - age2 < age3) #True
 print(age1 * age3 >= age2) #True
-print(age2 // age3 <=age1) #True
+print(age2 // age3 <= age1) #True
 print(age3 % age1 == age2) #False
 print(age2 ** age3 != age1) #True
 
@@ -25,7 +25,7 @@ num3=30
 print(num1 + num2 > num3) #False
 print(num1 - num2 < num3) #True
 print(num1 * num3 >= num2) #True
-print(num2 // num3 <=num1) #True
+print(num2 // num3 <= num1) #True
 print(num3 % num1 == num2) #True
 print(num2 ** num3 != num1) #True
 
@@ -36,7 +36,7 @@ age3=16
 print(age1 + age2 > age3) #True
 print(age1 - age2 < age3) #False
 print(age1 * age3 >= age2) #True
-print(age2 // age3 <=age1) #True
+print(age2 // age3 <= age1) #True
 print(age3 % age1 == age2) #False
 print(age2 ** age3 != age1) #True
 
@@ -47,7 +47,7 @@ num3=41
 print(num1 + num2 > num3) #False
 print(num1 - num2 < num3) #True
 print(num1 * num3 >= num2) #True
-print(num2 // num3 <=num1) #True
+print(num2 // num3 <= num1) #True
 print(num3 % num1 == num2) #True
 print(num2 ** num3 != num1) #True
 
---
+++ day16/homework/homework.py
@@ -0,0 +1,1 @@
+#no homework, had a group work. only sololearn
